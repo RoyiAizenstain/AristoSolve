7b1112c117aa3c67ac58e41d1187797405a3c743
Royi Aizen <royi.aizen@gmail.com>
2026-06-05 15:16:49 +0300

    Refactor server.js to implement CORS middleware and update API route paths for consistency

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const logger = require('./middleware/logger');
 app.use(express.json());
 app.use(logger);
 
-app.use('/users', require('./routes/users'));
-app.use('/problems', require('./routes/problems'));
-app.use('/conversations', require('./routes/conversations'));
-app.use('/evaluations', require('./routes/evaluations'));
-app.use('/progress', require('./routes/progress'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-user-role, x-user-id');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/problems', require('./routes/problems'));
+app.use('/api/conversations', require('./routes/conversations'));
+app.use('/api/evaluations', require('./routes/evaluations'));
+app.use('/api/progress', require('./routes/progress'));
+app.use('/api/settings', require('./routes/settings'));
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
